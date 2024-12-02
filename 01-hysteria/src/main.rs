ea10b9511e4921f4778993d0003417e4ba260b39
lh317 <46079517+lh317@users.noreply.github.com>
2024-12-02 00:03:14 -0500

    Day 1

---
+++ 01-hysteria/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+use eyre::{eyre, OptionExt, Result};
+
+fn main() -> Result<()> {
+    let mut args = std::env::args();
+    let fname = args.nth(1).ok_or_eyre("filename was no provided")?;
+    let body = fs::read_to_string(fname.as_str())?;
+    let mut left = vec![];
+    let mut right = vec![];
+    for (lineno, line) in body.lines().enumerate() {
+        let lineno = lineno + 1;
+        for (i, part) in line.split_whitespace().enumerate() {
+            match i {
+                0 => left.push(part.parse::<i32>()?),
+                1 => right.push(part.parse::<i32>()?),
+                _ => Err(eyre!("{fname}:{lineno}: too many numbers"))?,
+            };
+        }
+    }
+    left.sort();
+    right.sort();
+    let sum: i32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+    println!("{}", sum);
+    let similiarity: i32 = left.iter().try_fold(0, |acc, l| {
+        i32::try_from(right.iter().filter(|r| l == *r).count()).map(|r| acc + l * r)
+    })?;
+    println!("{}", similiarity);
+    Ok(())
+}
